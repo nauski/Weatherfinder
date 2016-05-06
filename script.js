48c059d919cb3a5ab9556d86bccfa1fa5f64b0c4
nauski <nauski@nauski.com>
2016-05-06 04:53:43 +0300

    added geolocation feature and a p tag for it

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-        $(document).ready(function(){
+$(document).ready(function(){
 
-                        var api = "http://api.openweathermap.org/data/2.5/weather?id=658225&appid=c90c8aa90a2c7f2b03f4a8ccb7d436a8";
-                        $.getJSON(api, function(data){
-                                alert(data.coord.lon);
-
+	    if (navigator.geolocation) {
+      		  navigator.geolocation.getCurrentPosition(function(pos) {
+  	 			$("#pos").html("lat: " +  pos.coords.latitude + "</br>" +
+					       "long: " + pos.coords.longitude);			
+    	
 });
+}
 
+
+
+     var api = "http://api.openweathermap.org/data/2.5/weather?id=658225&appid=c90c8aa90a2c7f2b03f4a8ccb7d436a8";
+     $.getJSON(api, function(data){
+//   alert(data.coord.lon);
+
+     });
 });
